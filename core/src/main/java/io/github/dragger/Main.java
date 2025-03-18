9f75afb02835bf9c22c24269631e4dcdb841d68c
Nandagopal SB <nandagopal.nsb@gmail.com>
2025-03-18 22:08:18 +0530

    added grids

--- core/src/main/java/io/github/dragger/Main.java
+++ core/src/main/java/io/github/dragger/Main.java
@@ -1,38 +1,40 @@
 package io.github.dragger;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
+    ShapeRenderer shape;
     Rectangle rect;
     private Texture image;
+
     @Override
     public void create() {
+        shape = new ShapeRenderer();
         batch = new SpriteBatch();
-        image = new Texture("drawing.png");
-        rect = new Rectangle(10,10,10,10);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-//        for(int i = 0 ; i<20; i++){
-//            for(int j = 0 ; j<20 ; j++){
-//                batch.draw(image, i*7, j*6);
-//            }
-//        }
-        batch.draw(image,700,700);
-
-        batch.end();
+        shape.begin(ShapeType.Line);
+        shape.setColor(Color.RED);
+        int gridSize = 100;
+        for (int x = 0; x < 800; x += gridSize) {
+            for (int y = 0; y < 600; y += gridSize) {
+                shape.rect(x, y, gridSize, gridSize);
+            }
+        }
+        shape.end();
     }
+
     @Override
     public void dispose() {
         batch.dispose();
